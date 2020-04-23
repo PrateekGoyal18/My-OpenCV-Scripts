ffbfc263f41c346d65673d465de5dae790682f0d
Prateek Goyal <prateekg045@gmail.com>
2020-04-23 22:27:52 +0530

    First commit

--- image_blur.py
+++ image_blur.py
@@ -4,7 +4,7 @@ import cv2
 image = cv2.imread("image.jpg")
 (h, w, d) = image.shape
 
-# apply a Gaussian blur with a 11x11 kernel to the image to smooth it,
+# Gaussian blur with a 11x11 kernel to the image to smooth it,
 # useful when reducing high frequency noise
 blurred = cv2.GaussianBlur(image, (9,9), 0)
 cv2.imshow("Blurred", blurred)
