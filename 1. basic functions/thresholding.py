dbc201b669666065eef669adf9541fd8710b129c
Prateek Goyal <prateekg045@gmail.com>
2020-04-28 11:25:18 +0530

    Changed some codes

--- 1. basic functions/thresholding.py
+++ 1. basic functions/thresholding.py
@@ -1,13 +1,16 @@
 import cv2
 
+# read the image
 image = cv2.imread("images/coins.png")
 cv2.imshow("Original", image)
 cv2.waitKey(0)
 
+# convert the image to grayscale
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Gray", image)
 cv2.waitKey(0)
 
+# blurring the image to remove unwanted noise
 blurred = cv2.GaussianBlur(image, (5,5), 0)
 cv2.imshow("Blurred", blurred)
 cv2.waitKey(0)
@@ -18,5 +21,6 @@ ret, thresh = cv2.threshold(blurred, 155, 255, cv2.THRESH_BINARY_INV)
 cv2.imshow("Thresh", thresh)
 cv2.waitKey(0)
 
+# applying the mask for coins detection
 cv2.imshow("Coins", cv2.bitwise_and(image, image, mask=thresh))
 cv2.waitKey(0)
